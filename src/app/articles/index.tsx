8c3e101a820fe95cbe667f4201e4f4b0ddb30c4b
natsuno <linsukida@gmail.com>
2026-06-20 18:15:24 +0900

    update

--- src/app/articles/index.tsx
+++ src/app/articles/index.tsx
@@ -12,12 +12,14 @@ import Loading from "@/src/components/Loading";
 import NetworkError from "@/src/components/NetworkError";
 import {ArticleItem, ArticleResponse} from "@/src/types";
 import {Link, Stack} from "expo-router";
+import NoData from "@/src/components/NoData";
 
 export default function ArticlesIndex() {
     const { data, loading, error, refresh, onReload, onRefresh, setData } =
         useFetchData<ArticleResponse>('/articles')
 
     const articles = data?.articles ?? []
+    // const articles: ArrayLike<ArticleItem> | null | undefined = []
 
     if (loading) {
         return <Loading />
@@ -42,7 +44,7 @@ export default function ArticlesIndex() {
                 <Link asChild href={{ pathname: '/articles/[id]', params: {id: item.id} }}>
                     <TouchableWithoutFeedback>
                         <View style={styles.item}>
-                            <Image />
+                            <Image source={require('@/src/assets/images/list-light.png')} style={styles.image}/>
                             <View style={styles.titleWrapper}>
                                 <Text style={styles.title} numberOfLines={2}>
                                     {item.title}
@@ -65,6 +67,7 @@ export default function ArticlesIndex() {
             keyExtractor={(item) => item.id.toString()}
             renderItem={renderItem}
             ItemSeparatorComponent={renderSeparator}
+            ListEmptyComponent={<NoData/>}
             refreshControl={
             <RefreshControl 
                 refreshing={refresh}
@@ -78,16 +81,24 @@ export default function ArticlesIndex() {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
     },
     contentContainerStyle: {
+        flexGrow: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center',
     },
     image: {
-        height: 70,
-        width: 70,
+        height: 50,
+        width: 50,
         alignSelf: 'center',
+        marginRight: 10,
+        marginLeft: 10,
     },
     item: {
         paddingBottom: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     titleWrapper: {
         flex: 1,
